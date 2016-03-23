285c709c1b63a264d7b8f1e981793375ce0f2548
Jayden Bayangos-Brandt <s3485426@student.rmit.edu.au>
2016-03-23 14:10:16 +1100

    initial commit

---
+++ app/src/main/java/rmit/nearbyneeds/MainActivity.java
@@ -0,0 +1,13 @@
+package rmit.nearbyneeds;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
